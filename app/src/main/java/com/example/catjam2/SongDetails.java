7adafe1d472c6653c1069e49da1f0e4c3c1535d7
amarell <mujagicamar@gmail.com>
2021-06-06 19:34:06 +0200

    Home screen recommendations and creating playlists!

--- app/src/main/java/com/example/catjam2/SongDetails.java
+++ app/src/main/java/com/example/catjam2/SongDetails.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -91,6 +93,44 @@ public class SongDetails extends AppCompatActivity {
 
         } catch(IOException e){
             e.printStackTrace();
+        }
+    }
+
+    public void showDetailsOnGenius(View view){
+        String aName = artistName.getText().toString();
+        String sName = songName.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+        String url = "https://genius.com/";
+        String[] artistWords = aName.split(" ");
+        String[] songWords = sName.split(" ");
+        for(String word : artistWords){
+            url+=word+"-";
+        }
+        for(String word : songWords){
+            url+=word+"-";
+        }
+        url+="lyrics";
+
+        intent.setData(Uri.parse(url));
+
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
+
+
+    }
+
+    public void share(View view){
+        String aName = artistName.getText().toString();
+        String sName = songName.getText().toString();
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Hello, I am listening to "+sName+" by "+aName+" on CatJam! It is Amazing!");
+        sendIntent.setType("text/plain");
+
+        if(sendIntent.resolveActivity(getPackageManager())!=null){
+            startActivity(sendIntent);
         }
     }
 }
