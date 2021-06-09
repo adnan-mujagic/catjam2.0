3673cd0dbaa8d7a14d546902de69a686f6ee87e5
amarell <mujagicamar@gmail.com>
2021-06-09 17:29:10 +0200

    added splash

---
+++ app/src/main/java/com/example/catjam2/Splash.java
@@ -0,0 +1,42 @@
+package com.example.catjam2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        img = findViewById(R.id.logo);
+        img.setImageResource(R.drawable.ic_baseline_music_note_24);
+        img.setColorFilter(Color.CYAN);
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),LoginActivity.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
+    }
+}
