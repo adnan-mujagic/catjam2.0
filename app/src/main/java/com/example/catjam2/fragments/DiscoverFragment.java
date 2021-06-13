bd0deb102811cf38d8e6f72828c8badc0e487c3c
amarell <mujagicamar@gmail.com>
2021-06-13 18:49:00 +0200

    logout feature and user details screen

--- app/src/main/java/com/example/catjam2/fragments/DiscoverFragment.java
+++ app/src/main/java/com/example/catjam2/fragments/DiscoverFragment.java
@@ -1,29 +1,87 @@
 package com.example.catjam2.fragments;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.catjam2.R;
+import com.example.catjam2.activities.LoginActivity;
+import com.example.catjam2.activities.MainActivity;
+import com.example.catjam2.classes.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class DiscoverFragment extends Fragment {
-    ListView listView;
+
+    Button logoutButton;
+    ImageView profilePic;
+    TextView username,email;
     public static final int SONG_ID = -1;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_discover,container,false);
+
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        logoutButton = getView().findViewById(R.id.logout_button);
+        profilePic = getView().findViewById(R.id.profile_picture);
+        username = getView().findViewById(R.id.profile_username);
+        email = getView().findViewById(R.id.profile_email);
+
+
+        profilePic.setImageResource(R.drawable.ic_baseline_person_outline_24);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("users");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.child(MainActivity.username).getValue(User.class);
+                if(user==null){
+                    Toast.makeText(getContext(), "Something went wrong while contacting the database.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    username.setText(user.getUsername());
+                    email.setText(user.getEmail());
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
+
     /*
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
